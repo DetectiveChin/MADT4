9ab2e2228a53b4560f3b165f1821d9ac4c9bfb67
DetectiveChin <91797123+DetectiveChin@users.noreply.github.com>
2021-11-28 16:55:26 +0200

    madt4

---
+++ app/src/main/java/com/example/notes/NoteEditorActivity.java
@@ -0,0 +1,9 @@
+package com.example.notes;
+
+import android.app.Activity;
+import android.view.View;
+
+public class NoteEditorActivity extends Activity {
+    public void backToMain2Activity_Btn_Click(View view) {
+    }
+}
